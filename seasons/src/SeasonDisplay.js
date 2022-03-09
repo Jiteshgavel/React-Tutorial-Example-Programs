3c69a85b4ff37ce23d7ecb8aef16c5b02ef794c4
jiteshgavel <jiteshgavel000@gmail.com>
2022-03-10 00:22:17 +0530

    section 6 -> Season Application complete

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,6 +1,37 @@
 import React from "react";
-import  ReactDOM  from "react-dom";
-
-const Season = ()=>{
-    return <div>Season display</div>
+import './display.css';
+const seasonConfig = {
+    summer : {
+        text: "Let's hit the beach",
+        iconName:'fa-sun'
+    },
+    winter : {
+        text: "Burr, it is chilly",
+        iconName: 'fa-snowflake'
+            // < i class= "fa-thin fa-snowflake" ></>
+    }
+};
+const getSeason =(lat,month) =>{
+    if(month > 2 && month < 9){
+        return  lat > 0 ? 'summer' : 'winter';
+    }else{
+        return lat > 0 ? 'winter' : 'summer';
+    }
 }
+{/* <i class="snowflake icon"></i> */}
+const SeasonDisplay = (props)=>{
+    console.log(props.lat);
+    const season = getSeason(props.lat,new Date().getMonth());
+    // const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+    // const icon = season === 'winter' ? 'winter' : 'sun';
+
+   const { text,iconName} = seasonConfig[season];
+
+    console.log(season);
+    return (<div className={`season-display ${season}`}> <i className={`fas icon-left  ${iconName} `} />
+               <h1>{text}</h1>  
+        <i className={` fas icon-right ${iconName} `} />
+        </div>);
+};
+
+export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,41 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-
-class App extends React.Component{
-  constructor(props){
-    super(props);
-    // THIS IS ONLY TIME we do direct assignment
-    // to this.state
-    this.state={lat: null,errorMessage:''};
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // we call set set state !!!
-        this.setState({lat:position.coords.latitude});
-      },
-      (err) => {
-        this.setState({errorMessage:err.message});
-      }
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
     );
   }
+  // componentDidUpdate()
+  // {
+  //   console.log('My component was just updated -it rerendered')
+  // }
   // React says we have to define render
-  render(){
-  
-    
-      if(this.state.errorMessage && !this.state.lat)
-      {
-      return <div>Error : {this.state.errorMessage}</div>
-      }
-      if (this.state.errorMessage && this.state.lat) {
-        return <div> Latitude : {this.state.lat}</div>
-      }
-    return <div>Loading !</div>
-    // <div>
-    //     Latitude : {this.state.lat}<br></br>
-    //     Error : {this.state.errorMessage}
-    // </div>
 
+  renderContent() {
     
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error : {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    } else {
+      return <Spinner />;
+    }
+  }
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
   }
 }
 ReactDOM.render(<App />, document.querySelector("#root"));
